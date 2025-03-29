91994cf306e069601b567d11930ba459917812f2
Ankush Kawalkar <ankush.014k@gmail.com>
2025-03-29 16:59:16 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Welcome to Yoga For Life</h1>;
+  return (
+    <main style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Welcome to Yoga For Life</h1>
+    </main>
+  );
 }
