74d35c12f7316caea48f6759692fc5597ae591ad
Ankush Kawalkar <ankush.014k@gmail.com>
2025-03-29 16:28:54 +0530

    Initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enables static export
+  basePath: "/ankush014k.github.io", // Replace 'YOGAFORLIFE' with your repo name
+  images: {
+    unoptimized: true, // Necessary for GitHub Pages since it doesn't support Next.js image optimization
+  },
 };
 
 export default nextConfig;
