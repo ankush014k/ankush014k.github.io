4794e473885fdb8d6072b5a000d373953753922e
ankush014k <ankush.014k@gmail.com>
2025-03-31 17:26:15 +0530

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main style={{ padding: "20px", textAlign: "center" }}>
-      <h1>Welcome to Yoga For Life</h1>
-    </main>
-  );
-}
