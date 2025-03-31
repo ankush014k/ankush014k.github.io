425f45e21df811f9940b1260a77c1761b3f0bcd1
Ankush Kawalkar <ankush.014k@gmail.com>
2025-03-31 17:47:55 +0530

    added files

---
+++ src/components/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Gallery = () => {
+  return (
+    <div>
+      <h2>Gallery</h2>
+      <p>Check out photos from our yoga classes and events!</p>
+    </div>
+  );
+};
+
+export default Gallery;
